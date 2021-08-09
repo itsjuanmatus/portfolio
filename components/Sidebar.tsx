eeafdebb2c093507d52eebdba2c167ec64322c87
itsjuanmatus <juanmatusg16@gmail.com>
2021-08-08 19:35:21 -0600

    changed lightbulb to moon

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -93,20 +93,11 @@ export default function Sidebar() {
         <div className="inline-flex min-h-full">
           <img className="h-20 w-20 p-4" src="/favicon.svg" alt="logo" />
           {colorTheme === "light" ? (
-            <svg
+            <img
               onClick={() => setTheme("light")}
-              xmlns="http://www.w3.org/2000/svg"
-              className="w-7 bottom-0  text-white flex items-center justify-center"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M9.663 17h4.673M12 3v1m6.364 1.636l-.707.707M21 12h-1M4 12H3m3.343-5.657l-.707-.707m2.828 9.9a5 5 0 117.072 0l-.548.547A3.374 3.374 0 0014 18.469V19a2 2 0 11-4 0v-.531c0-.895-.356-1.754-.988-2.386l-.548-.547z"
-              />
-            </svg>
+              src="/images/moon_filled.svg"
+              className="w-8 bottom-0 text-black flex items-center justify-center"
+            />
           ) : (
             <img
               onClick={() => setTheme("dark")}
