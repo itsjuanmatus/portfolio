68564e726501aa86977e9ecafee5643e284a9a99
itsjuanmatus <juanmatusg16@gmail.com>
2021-10-14 22:01:06 -0600

    fixed sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 import useDarkMode from './useDarkMode'
 import { useEffect, useState } from 'react'
 import { Transition } from '@headlessui/react'
-import { motion } from 'framer-motion'
+import { AnimatePresence, motion, useCycle } from 'framer-motion'
 import * as Icon from 'react-bootstrap-icons'
 
 export default function Sidebar () {
@@ -12,7 +12,7 @@ export default function Sidebar () {
   }, [])
   const router = useRouter()
   const [colorTheme, setTheme]: any = useDarkMode()
-  const [isOpen, setIsOpen] = useState(false)
+  const [open, cycleOpen]: any = useCycle(false, true)
   return (
     <>
       <aside className='hidden lg:block justify-center fixed bg-white dark:bg-gray-background 7 h-screen w-1/6 max-w-1/6 pt-10'>
@@ -131,7 +131,7 @@ export default function Sidebar () {
                 className='h-20 w-20 p-4'
                 src='/favicon.svg'
                 alt='logo'
-                onClick={() => setIsOpen(!isOpen)}
+                onClick={() => cycleOpen()}
               />
             </motion.div>
           </Link>
@@ -162,14 +162,14 @@ export default function Sidebar () {
         </div>
         <motion.div whileTap={{ rotate: 360 }}>
           <button
-            onClick={() => setIsOpen(!isOpen)}
-            type='button'
+                onClick={() => cycleOpen()}
+                type='button'
             className='inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white '
             aria-controls='mobile-menu'
             aria-expanded='false'
           >
             <span className='sr-only'>Open main menu</span>
-            {!isOpen ? (
+            {!open ? (
               <motion.div
                 animate={{
                   originX: 54,
@@ -209,57 +209,58 @@ export default function Sidebar () {
         </motion.div>
       </div>
 
-      <Transition
-        show={isOpen}
-        enter='transition ease-out duration-100 transform'
-        enterFrom='opacity-0 scale-95'
-        enterTo='opacity-100 scale-100'
-        leave='transition ease-in duration-75 transform'
-        leaveFrom='opacity-100 scale-100'
-        leaveTo='opacity-0 scale-95'
-      >
-        {ref => (
+      <AnimatePresence>
+        {open && (
+          <motion.aside
+            initial={{ y: -600 }}
+            animate={{ y: 0 }}
+            exit={{
+              y: -600,
+              transition: { duration: 0.3 }
+            }}
+          >
+        
           <div className='lg:hidden' id='mobile-menu'>
             <div
-              ref={ref}
               className='px-2 pt-2 pb-3 space-y-1 sm:px-3 dark:bg-gray-background'
             >
               <Link href='/'>
                 <a
-                  onClick={() => setIsOpen(!isOpen)}
-                  className='text-gray-background dark:text-white font-semibold block px-3 py-2 rounded-md text-base'
+                onClick={() => cycleOpen()}
+                className='text-gray-background dark:text-white font-semibold block px-3 py-2 rounded-md text-base'
                 >
                   Home
                 </a>
               </Link>
               <Link href='/about'>
                 <a
-                  onClick={() => setIsOpen(!isOpen)}
-                  className='text-gray-background dark:text-white font-semibold block px-3 py-2 rounded-md text-base'
+                onClick={() => cycleOpen()}
+                className='text-gray-background dark:text-white font-semibold block px-3 py-2 rounded-md text-base'
                 >
                   About
                 </a>
               </Link>
               <Link href='/work'>
                 <a
-                  onClick={() => setIsOpen(!isOpen)}
-                  className='text-gray-background dark:text-white font-semibold block px-3 py-2 rounded-md text-base'
+                onClick={() => cycleOpen()}
+                className='text-gray-background dark:text-white font-semibold block px-3 py-2 rounded-md text-base'
                 >
                   Work
                 </a>
               </Link>
               <Link href='/contact'>
                 <a
-                  onClick={() => setIsOpen(!isOpen)}
-                  className='text-gray-background dark:text-white font-semibold block px-3 py-2 rounded-md text-base'
+                onClick={() => cycleOpen()}
+                className='text-gray-background dark:text-white font-semibold block px-3 py-2 rounded-md text-base'
                 >
                   Contact
                 </a>
               </Link>
             </div>
           </div>
+          </motion.aside>
         )}
-      </Transition>
+      </AnimatePresence>
     </>
   )
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,7 @@ import Sidebar from "../components/Sidebar";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div className="overflow-x-hidden">
+    <div className="overflow-x-hidden dark:bg-gray-background">
       <Sidebar />
       <Component {...pageProps} />
     </div>
